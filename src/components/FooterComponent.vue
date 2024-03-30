8f52b865b0a9855cff123fad54e1280a88749ee4
Maaz Chowdhry <maazchowdhry2001@gmail.com>
2024-03-30 17:22:48 +0000

    Update App.vue and index.html, remove unused files

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -2,7 +2,7 @@
     <footer class="bg-white dark:bg-gray-900">
         <div class="container flex flex-col items-center justify-between p-6 mx-auto space-y-4 sm:space-y-0 sm:flex-row">
             
-            <a href="#" class="flex -mx-2">
+            <a href="#" class="flex items-center">
                 <img class="h-6" src="../images/Bitcoin_46999.ico">
                 <h3 class="text-xl text-gray-600 dark:text-gray-300">BET-COIN.com</h3>
             </a>
