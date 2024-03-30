591ec12211a69200ab2586ea9e90dda4dbaa0917
Maaz Chowdhry <maazchowdhry2001@gmail.com>
2024-03-30 23:51:58 +0000

    Add Plotly.js library and MainComponent

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 import NavbarComponent from './components/NavbarComponent.vue';
+import MainComponent from './components/MainComponent.vue';
 import FooterComponent from './components/FooterComponent.vue';
 </script>
 
 <template>
   <NavbarComponent />
-  
+  <MainComponent />
   <FooterComponent />
 </template>
 
